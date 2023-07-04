6d316a0f6d93ea08f9bfbff5fe6b0e6395b81d78
MusaAbdillah <musaabdillahm9@gmail.com>
2023-07-04 21:40:40 +0700

    initalize react form

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Form from "./components/Form";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  return (
+    <div className="container mx-auto px-4">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
